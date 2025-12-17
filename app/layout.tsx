4f8e13770db40ca475a6289f3890fdc59bb91fd5
fangg19 <alexjohnny6@gmail.com>
2025-12-17 09:05:32 +0200

    packages updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
+import "./global.css";
 import Navbar from "./components/sections/Navbar";
-import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
 import { keywords } from "./utils/keywords";
@@ -8,7 +8,7 @@ import type { Viewport } from "next";
 
 export const metadata: Metadata = {
   title: "alexjonny",
-  description: "I'm Jonny and I like to code I think.",
+  description: "i'm Jonny and i think i like code",
   authors: [
     { name: "Serban Alexandru Jonny", url: "https://alexjonny.com" },
     { name: "fangg19", url: "https://alexjonny.com" },
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
   },
   openGraph: {
     title: "alexjonny",
-    description: "I'm Jonny and I like to code I think.",
+    description: "i'm Jonny and i think i like code",
     url: "https://alexjonny.com",
     siteName: "alexjonny",
     images: [
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
     locale: "en_GB",
     alternateLocale: ["en_US"],
     countryName: "Romania",
-    emails: ["hello@alexjonny.com", "alexjohnny6@gmail.com"],
+    emails: ["hello@alexjonny.com"],
     type: "website",
   },
 };
